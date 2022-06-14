e2b56b6cb6b2fed530408658e51a16d0efb83185
Chapmaan <106259617+Chapmaan@users.noreply.github.com>
2022-06-14 08:20:56 -0400

    Add files via upload

---
+++ Rat.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Rat here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Rat extends Enemy
+{
+    //DISCLAIMER: ALL STATS ARENT YET BASED OFF OF CARDS
+    
+    //stats
+    //hp = 9
+    //atk = 2
+    //def  (idk if this even exists yet but) = 0
+    //spec atk = atk + 1 to 2 | OR | atk + 1 to 2 turns of poison for 1 each turn
+    
+    //coding stats
+    //Choose == 0;
+    /**
+     * Act - do whatever the Rat wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
