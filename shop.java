26f5f5c9d4f5cb51b96fea8af2709c8ab8881557
cartyg <106537070+cartyg@users.noreply.github.com>
2022-06-15 10:54:45 -0400

    added shop world

---
+++ shop.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class shop here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class shop extends World
+{
+
+    /**
+     * Constructor for objects of class shop.
+     * 
+     */
+    public shop()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
