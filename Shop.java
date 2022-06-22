567d7c6c8db2a35d1f164d643e12e9120890a6d6
J-Bird22 <jessejmoore@yahoo.ca>
2022-06-22 08:01:54 -0400

    .
    RandomFiles GreenFoot Gave Me (They Look Useful So Im Commiting Them)

--- Shop.java
+++ Shop.java
@@ -1,39 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class shop here.
+ * Write a description of class Shop here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class shop extends World
+public class Shop extends Actor
 {
-
     /**
-     * Constructor for objects of class shop.
-     * 
+     * Act - do whatever the Shop wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public shop()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
-        prepare();
-    }
-    
-    /**
-     * Prepare the world for the start of the program.
-     * That is: create the initial objects and add them to the world.
-     */
-    private void prepare()
+    public void act() 
     {
-        StoreKeeper storeKeeper = new StoreKeeper(300, 300);
-        addObject(storeKeeper,251,195);
-
-        BlackLine blackLine = new BlackLine(300, 10);
-        addObject(blackLine,269,256);
-
-        Shopbutton shopbutton = new Shopbutton(150, 150);
-        addObject(shopbutton,88,100);
-        shopbutton.setLocation(81,69);
-    }
+        // Add your action code here.
+    }    
 }
