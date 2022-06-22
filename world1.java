8bdce43c012ece0acad551856a96ebb3ba420d94
J-Bird22 <jessejmoore@yahoo.ca>
2022-06-22 08:32:45 -0400

    FixedBrokenBrokeness

--- world1.java
+++ world1.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class world1 extends Actor
+public class World1 extends Actor
 {
     /**
      * Act - do whatever the World1 wants to do. This method is called whenever
